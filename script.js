78e147f0881e7f9f7df45ccde0ec848bf612fcc5
lost <danielschoolxd@gmail.com>
2023-12-19 17:54:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const canvas = document.getElementById("drawing-board");
+const toolbar = document.getElementById("toolbar");
+const ctx = canvas.getContext("2d");
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+let savedImageData;
+
+toolbar.addEventListener("click", (e) => {
+  if (e.target.id === "clear") {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
+
+toolbar.addEventListener("change", (e) => {
+  if (e.target.id === "stroke") {
+    ctx.strokeStyle = e.target.value;
+  }
+
+  if (e.target.id === "lineWidth") {
+    lineWidth = e.target.value;
+  }
+});
+
+toolbar.addEventListener("click", (e) => {
+    if (e.target.id === "save") {
+      savedImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    }
+  });
+  
+  toolbar.addEventListener("click", (e) => {
+    if (e.target.id === "restore") {
+      if (savedImageData) {
+        ctx.putImageData(savedImageData, 0, 0);
+      }
+    }
+  });
+
+const draw = (e) => {
+  if (!isPainting) {
+    return;
+  }
+
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = "round";
+
+  ctx.lineTo(e.clientX, e.clientY - canvasOffsetY);
+  ctx.stroke();
+};
+
+canvas.addEventListener("mousedown", (e) => {
+  isPainting = true;
+  startX = e.clientX;
+  startY = e.clientY;
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPainting = false;
+  ctx.stroke();
+  ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
